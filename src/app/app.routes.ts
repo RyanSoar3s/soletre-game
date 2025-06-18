523323a1f728e43b8950fc8a27a14b226dda834c
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-18 14:08:03 -0300

    feat: add server-side routing configuration and enhance app routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeScreenComponent } from '@components/home-screen/home-screen.component';
+import { GameComponent } from '@components/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: HomeScreenComponent
+
+  },
+  {
+    path: "soletre",
+    component: GameComponent
+
+  },
+  {
+    path: "**",
+    redirectTo: ""
+
+  }
+
+];
