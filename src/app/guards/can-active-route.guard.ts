51f28c8e1f6185c7973e9b9ee193e4686e919f6e
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-21 18:34:54 -0300

    refactor(can-active-route.guard): replace RequestApiService with GameService for game state validation

--- src/app/guards/can-active-route.guard.ts
+++ src/app/guards/can-active-route.guard.ts
@@ -1,19 +1,19 @@
 import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
-import { RequestApiService } from '@services/request-api.service';
+import { GameService } from '@services/game.service';
 import { Router } from '@angular/router';
 
 export const canActiveRouteGuard: CanActivateFn = () => {
   const router = inject(Router);
-  const requestApiService = inject(RequestApiService);
-  const data = requestApiService.getSoletreGame();
+  const gameService = inject(GameService);
+  const isGameSaved = gameService.isGameSaved();
 
-  if (!data) {
+  if (!isGameSaved) {
     router.navigate(['/']);
     return false;
 
   }
 
   return true;
-  
+
 };
