3f2f117265973c78bcf258d21f2d23f2e2b6fad1
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-21 18:36:55 -0300

    refactor(soletre-game-data.resolver): enhance game data resolver with platform check and game state validation

--- src/app/resolvers/soletre-game-data.resolver.ts
+++ src/app/resolvers/soletre-game-data.resolver.ts
@@ -1,14 +1,26 @@
 import { ResolveFn } from '@angular/router';
-import { inject } from '@angular/core';
+import { inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { RequestApiService } from '@services/request-api.service';
+import { GameService } from '@services/game.service';
 
 export const soletreGameDataResolver: ResolveFn<void> = () => {
+  const platformId = inject(PLATFORM_ID);
+  if (!isPlatformBrowser(platformId)) return;
+
+  const gameService = inject(GameService);
+  const isGameSaved = gameService.isGameSaved();
+
   const requestApiService = inject(RequestApiService);
-  const data = requestApiService.getSoletreGame();
+  const date = new Date().getDate();
 
-  if (!data) {
-    requestApiService.requestApi();
+  if (isGameSaved && gameService.getGame()?.date === date) return;
 
-  }
+  if (gameService.getGame()?.date !== date) gameService.clearGame();
+
+  requestApiService.requestSoletreGameApi().subscribe((data) => {
+    gameService.saveGame(data);
+
+  });
 
 };
