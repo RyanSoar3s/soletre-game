cb0232ed29d25649c76bfca9492d624ce0454643
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-21 23:18:31 -0300

    fix(api/index.js): correct import path for server module

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 export default async (req, res) => {
-  const { app } = await import("../dist/soletre/server.mjs");
+  const { app } = await import("../dist/soletre/server/server.mjs");
   return app(req, res);
 
 };
