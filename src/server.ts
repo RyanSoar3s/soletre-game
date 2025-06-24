b9e7566b5990203b18c1464b51dcf6e7f33e8b6e
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-24 17:39:04 -0300

    refactor(server): remove unused isMainModule import and comment out server start logic

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine, isMainModule } from '@angular/ssr/node';
+import { CommonEngine } from '@angular/ssr/node';
 import express from 'express';
 import cors from 'cors'
 import { dirname, join, resolve } from 'node:path';
@@ -128,11 +128,11 @@ app.get('**', (req, res, next) => {
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+// if (isMainModule(import.meta.url)) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, () => {
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
 export default app;
