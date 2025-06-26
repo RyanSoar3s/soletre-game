7c8909a3b6a9ed3fba957d8a3e27e386fa3e55c1
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-26 16:48:11 -0300

    feat: refactor requestSoletreGameApi to return ResponseApi type

--- src/app/services/request-api.service.ts
+++ src/app/services/request-api.service.ts
@@ -1,8 +1,8 @@
 import { inject, Injectable } from '@angular/core';
-import { SoletreGame } from '@models/soletre-game.model';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '@environments/environment';
 import { Observable } from 'rxjs';
+import { ResponseApi } from '@models/response-api.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,8 @@ import { Observable } from 'rxjs';
 export class RequestApiService {
   private http = inject(HttpClient);
 
-  requestSoletreGameApi(): Observable<{ message: string, game: SoletreGame | null, error?: unknown }> {
-    return this.http.get(`${environment.apiUrl}/api/wordlist`) as Observable<{ message: string, game: SoletreGame | null, error?: unknown }>;
-
-  }
-
-  requestCheckWordApi(word: string): Observable<{ isValid: boolean, word: string | undefined, words: string[] }> {
-    return this.http.post(`${environment.apiUrl}/api/wordlist/check-word`, { word }) as Observable<{ isValid: boolean, word: string | undefined, words: string[] }>;
+  requestSoletreGameApi(): Observable<ResponseApi> {
+    return this.http.get(`${environment.apiUrl}/api/wordlist`) as Observable<ResponseApi>;
 
   }
 
