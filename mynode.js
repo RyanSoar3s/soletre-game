66ea8b6643c23b575a30791d3163b6b1c939f03f
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-26 17:28:33 -0300

    fix: correct formatting of environment export in mynode.js

--- mynode.js
+++ mynode.js
@@ -1,14 +1,18 @@
 import { writeFileSync } from 'fs';
 import { join } from 'path';
+import { fileURLToPath } from 'url';
 
 const content = `
-export const environment = {
-  production: true,
-  apiUrl: "https://soletre-game.vercel.app",
-  CLIENT_SECRET_KEY: '${process.env.CLIENT_SECRET_KEY}'
+  export const environment = {
+    production: true,
+    apiUrl: "https://soletre-game.vercel.app",
+    CLIENT_SECRET_KEY: '${process.env.CLIENT_SECRET_KEY}'
 
-};
+  };
 `;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 writeFileSync(join(__dirname, 'src/environments/environment.ts'), content);
 console.log('✅ environment.ts gerado com sucesso');
