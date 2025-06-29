a92cb7caf95f1717522cbb0fb9cf1c128826d880
RyanSoar3s <ryansoares.dev@gmail.com>
2025-06-29 11:51:32 -0300

    feat: add HowToPlayModal component with HTML, CSS, and unit tests

---
+++ src/app/components/how-to-play-modal/how-to-play-modal.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ModalComponent } from '@shared/modal/modal.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faQuestion } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-how-to-play-modal',
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    ModalComponent
+
+  ],
+  templateUrl: './how-to-play-modal.component.html',
+  styleUrl: './how-to-play-modal.component.css'
+})
+export class HowToPlayModalComponent {
+  protected readonly faQuestion = faQuestion;
+  protected readonly rules = [
+    "Toda palavra deve possuir no mínimo 4 letras.",
+    "Toda palavra deve possuir a letra central.",
+    "Não é permitido: palavras com hífen, pronomes, preposições e palavrões",
+    "Não é permitido palavras com plural. EX.: CARROS, MOEDAS...",
+    "É permitido verbos no infinitivo. EX.: JOGAR, COMER, VESTIR...",
+    "É permitido palavras no masculino e/ou feminino. EX.: MOÇO e MOÇA"
+
+  ];
+
+}
